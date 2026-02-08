b0d433575f23e07f4e53f8557ac479747e45b61a
Seif Mohamed <seif.eissa7797@gmail.com>
2026-02-08 20:56:31 +0200

    Restructure project: .NET 9 API, Docker, OpenTelemetry

--- DevHabit/DevHabit.Api/Controllers/WeatherForecastController.cs
+++ DevHabit/DevHabit.Api/Controllers/WeatherForecastController.cs
@@ -1,7 +1,7 @@
-using Microsoft.AspNetCore.Mvc;
 using System.Security.Cryptography;
+using Microsoft.AspNetCore.Mvc;
 
-namespace DevHabit.API.Controllers;
+namespace DevHabit.Api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
@@ -22,12 +22,13 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
+        _logger.LogInformation("Fetching weather forecast");
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = RandomNumberGenerator.GetInt32(-20, 55),
-            Summary = Summaries[RandomNumberGenerator.GetInt32(Summaries.Length)]
-        })
-        .ToArray();
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = RandomNumberGenerator.GetInt32(-20, 55),
+                Summary = Summaries[RandomNumberGenerator.GetInt32(Summaries.Length)]
+            })
+            .ToArray();
     }
 }
