13526d2f82ecc6260c7fb1be440d9a476d54705a
Seif Mohamed <seif.eissa7797@gmail.com>
2026-02-11 19:36:24 +0200

    Centralized exception handling with ProblemDetails

--- DevHabit/DevHabit.Api/Program.cs
+++ DevHabit/DevHabit.Api/Program.cs
@@ -1,5 +1,6 @@
 using DevHabit.Api.Database;
 using DevHabit.Api.Extentions;
+using DevHabit.Api.Middleware;
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations;
@@ -33,6 +34,9 @@ builder.Services.AddProblemDetails(options =>
         context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
     };
 });
+
+builder.Services.AddExceptionHandler<ValidationExceptionHandler>();
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 
 builder.Services.AddOpenApi();
 
@@ -73,6 +77,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseExceptionHandler();
 
 app.MapControllers();
 
