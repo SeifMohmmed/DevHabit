4ed13fc9b6f4885ea2b377c1841ca3f27839e973
Seif Mohamed <seif.eissa7797@gmail.com>
2026-02-28 17:31:26 +0200

    Add Scalar UI with OpenApi

--- src/DevHabit/DevHabit.Api/Program.cs
+++ src/DevHabit/DevHabit.Api/Program.cs
@@ -2,6 +2,7 @@ using DevHabit.Api;
 using DevHabit.Api.Extensions;
 using DevHabit.Api.Middleware;
 using DevHabit.Api.Settings;
+using Scalar.AspNetCore;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +25,11 @@ if (app.Environment.IsDevelopment())
 
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.MapScalarApiReference(options =>
+    {
+        options.WithOpenApiRoutePattern("/swagger/1.0/swagger.json");
+    });
 
     await app.ApplyMigrationsAsync();
     await app.SeedInitialDataAsync();
